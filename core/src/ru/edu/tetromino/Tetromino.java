4003b5c05510b59f7d873f72fad8ad7544e3e921
Michael <43596340+KolyanPie@users.noreply.github.com>
2019-02-16 19:26:56 +0300

    Tetromino

--- core/src/ru/edu/tetromino/Tetromino.java
+++ core/src/ru/edu/tetromino/Tetromino.java
@@ -2,9 +2,9 @@ package ru.edu.tetromino;
 
 import ru.edu.tetromino.visual.MainScreen;
 
-public class Game extends com.badlogic.gdx.Game {
+public class Tetromino extends com.badlogic.gdx.Game {
     @Override
     public void create() {
-        setScreen(new MainScreen());
+        setScreen(new MainScreen(this));
     }
 }
